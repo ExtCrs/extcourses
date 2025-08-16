a2b149b22d7d3e2a05cc3138ab476e3766a767c1
Vladimir Amosov <219029441+ExtCrs@users.noreply.github.com>
2025-08-16 13:19:03 +0500

    Update LessonMenu.js

--- components/displays/current_course/LessonMenu.js
+++ components/displays/current_course/LessonMenu.js
@@ -1,9 +1,9 @@
 
 export default function LessonMenu({ totalLessons, activeLesson, setActiveLesson, getBadge }) {
   return (
-    <ul className="menu menu-sm menu-horizontal bg-base-200 rounded-box mb-6 gap-px">
+    <ul className={`menu menu-sm menu-horizontal mt-2 border-t border-secondary/10 mb-6 gap-px grid lg:grid-cols-3 grid-cols-12 w-full`}>
       {Array.from({ length: totalLessons }, (_, i) => i + 1).map((num) => (
-        <li key={num}>
+        <li key={num} className="">
           <button
             className={num === activeLesson ? "menu-active !bg-accent rounded-full border border-accent font-bold shadow-lg" : "rounded-full border border-transparent"}
             onClick={() => setActiveLesson(num)}
